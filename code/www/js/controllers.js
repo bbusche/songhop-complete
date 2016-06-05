87dc262359691863f87c1579bbf6e6df7a03725b
Brian Busche <brian@rendition.net>
2016-06-05 13:01:20 -0500

    Complete Songhop app tutorial code from Thinkster

--- code/www/js/controllers.js
+++ code/www/js/controllers.js
@@ -80,7 +80,7 @@ Controller for the favorites page
 .controller('FavoritesCtrl', function($scope, $window, User) {
     // get the list of our favorites from the user service
     $scope.favorites = User.favorites;
-
+    $scope.username = User.username;
     $scope.removeSong = function(song, index) {
         User.removeSongFromFavorites(song, index);
     }
@@ -94,7 +94,7 @@ Controller for the favorites page
 /*
 Controller for our tab bar
 */
-.controller('TabsCtrl', function($scope, User, Recommendations) {
+.controller('TabsCtrl', function($scope, $window, User, Recommendations) {
     // Music Playing stuff - stop audio when going to favorites page
     $scope.enteringFavorites = function() {
       User.newFavorites = 0;
@@ -105,5 +105,31 @@ Controller for our tab bar
       Recommendations.init();
     }
 
+    $scope.logout = function() {
+      User.destroySession();
+
+      // instead of using $state.go, we're going to redirect.
+      // reason: we need to ensure views aren't cached.
+      $window.location.href = 'index.html';
+    }
+
     $scope.favCount = User.favoriteCount;
+})
+
+/*
+Controller for our login page
+*/
+.controller('SplashCtrl', function($scope, $state, User) {
+  // attempt to signup/login via User.auth
+  $scope.submitForm = function(username, signingUp) {
+    User.auth(username, signingUp).then(function(){
+      // session is now set, so lets redirect to discover page
+      $state.go('tab.discover');
+
+    }, function() {
+      // error handling here
+      alert('Hmm... try another username.');
+
+    });
+  }
 });
--- code/www/js/services.js
+++ code/www/js/services.js
@@ -1,9 +1,80 @@
-angular.module('songhop.services', [])
-    .factory('User', function() {
+angular.module('songhop.services', ['ionic.utils'])
+    .factory('User', function($http, $q, $localstorage, SERVER) {
 
         var o = {
+            username: false,
+            session_id: false,
             favorites: [],
             newFavorites: 0
+        }
+
+        // attempt login or signup
+        o.auth = function(username, signingUp) {
+
+            var authRoute;
+
+            if (signingUp) {
+                authRoute = 'signup';
+            } else {
+                authRoute = 'login'
+            }
+
+            return $http.post(SERVER.url + '/' + authRoute, {username: username})
+                .success(function(data) {
+                    o.setSession(data.username, data.session_id, data.favorites);
+                });
+        }
+
+        // set session data
+        o.setSession = function(username, session_id, favorites) {
+            if (username) o.username = username;
+            if (session_id) o.session_id = session_id;
+            if (favorites) o.favorites = favorites;
+
+            // set data in localstorage object
+            $localstorage.setObject('user', {
+                username: username,
+                session_id: session_id
+            });
+        }
+
+        // check if there's a user session present
+        o.checkSession = function() {
+          var defer = $q.defer();
+
+          if (o.session_id) {
+            // if this session is already initialized in the service
+            defer.resolve(true);
+
+          } else {
+            // detect if there's a session in localstorage from previous use.
+            // if it is, pull into our service
+            var user = $localstorage.getObject('user');
+
+            if (user.username) {
+              // if there's a user, lets grab their favorites from the server
+              o.setSession(user.username, user.session_id);
+              o.populateFavorites().then(function() {
+                defer.resolve(true);
+              });
+
+            } else {
+              // no user info in localstorage, reject
+              defer.resolve(false);
+            }
+
+          }
+
+          return defer.promise;
+        }
+
+        // wipe out our session data
+        o.destroySession = function() {
+           $localstorage.setObject('user', {});
+           o.username = false;
+           o.session_id = false;
+           o.favorites = [];
+           o.newFavorites = 0;
         }
 
         o.addSongToFavorites = function(song) {
@@ -13,6 +84,12 @@ angular.module('songhop.services', [])
             // add to favorites array
             o.favorites.unshift(song);
             o.newFavorites++;
+
+            // persist this to the server
+            return $http.post(SERVER.url + '/favorites', {
+                session_id: o.session_id,
+                song_id: song.song_id
+            });
         }
 
         o.removeSongFromFavorites = function(song, index) {
@@ -21,10 +98,34 @@ angular.module('songhop.services', [])
 
             // add to favorites array
             o.favorites.splice(index, 1);
+
+            // persist this to the server
+            return $http({
+                method: 'DELETE',
+                url: SERVER.url + '/favorites',
+                params: {
+                    session_id: o.session_id,
+                    song_id: song.song_id
+                }
+            });
+        }
+
+        // gets the entire list of this user's favs from server
+        o.populateFavorites = function() {
+            return $http({
+                method: 'GET',
+                url: SERVER.url + '/favorites',
+                params: {
+                    session_id: o.session_id
+                }
+            }).success(function(data) {
+                // merge data into the queue
+                o.favorites = data;
+            });
         }
 
         o.favoriteCount = function() {
-          return o.newFavorites;
+            return o.newFavorites;
         }
 
         return o;
@@ -53,61 +154,61 @@ angular.module('songhop.services', [])
       !!! First line of discover controller changed from Recommendations.getNextSongs() to Recommendations.init()
     */
     o.init = function() {
-      if (o.queue.length === 0) {
-        // if there's nothing in the queue, fill it.
-        // this also means that this is the first call of init.
-        return o.getNextSongs();
+        if (o.queue.length === 0) {
+            // if there's nothing in the queue, fill it.
+            // this also means that this is the first call of init.
+            return o.getNextSongs();
 
-      } else {
-        // otherwise, play the current song
-        return o.playCurrentSong();
-      }
+        } else {
+            // otherwise, play the current song
+            return o.playCurrentSong();
+        }
     }
 
     o.getNextSongs = function() {
-      return $http({
-        method: 'GET',
-        url: SERVER.url + '/recommendations'
-      }).success(function(data){
-        // merge data into the queue
-        o.queue = o.queue.concat(data);
-      });
+        return $http({
+            method: 'GET',
+            url: SERVER.url + '/recommendations'
+        }).success(function(data) {
+            // merge data into the queue
+            o.queue = o.queue.concat(data);
+        });
     }
 
     o.nextSong = function() {
-      // pop the index 0 off
-      o.queue.shift();
+        // pop the index 0 off
+        o.queue.shift();
 
-      // Music playing stuff
-      o.haltAudio();
+        // Music playing stuff
+        o.haltAudio();
 
-      // low on the queue? lets fill it up
-      if (o.queue.length <= 3) {
-        o.getNextSongs();
-      }
+        // low on the queue? lets fill it up
+        if (o.queue.length <= 3) {
+            o.getNextSongs();
+        }
 
     }
 
     // Music playing stuff
     o.playCurrentSong = function() {
-      var defer = $q.defer();
+        var defer = $q.defer();
 
-      // play the current song's preview
-      media = new Audio(o.queue[0].preview_url);
+        // play the current song's preview
+        media = new Audio(o.queue[0].preview_url);
 
-      // when song loaded, resolve the promise to let controller know.
-      media.addEventListener("loadeddata", function() {
-        defer.resolve();
-      });
+        // when song loaded, resolve the promise to let controller know.
+        media.addEventListener("loadeddata", function() {
+            defer.resolve();
+        });
 
-      media.play();
+        media.play();
 
-      return defer.promise;
+        return defer.promise;
     }
 
     // used when switching to favorites tab
     o.haltAudio = function() {
-      if (media) media.pause();
+        if (media) media.pause();
     }
 
     return o;
